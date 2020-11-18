7423f046b611685cbea009eff6d6e190874b7ed9
siddhantkushwaha <k16.siddhant@gmail.com>
2020-11-18 19:00:22 +0530

    Adding components to let the app be default sms app

---
+++ app/src/main/java/com/siddhantkushwaha/carolyn/service/MMSReceiver.kt
@@ -0,0 +1,16 @@
+package com.siddhantkushwaha.carolyn.service
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+
+class MMSReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        val tag = this::class.java.toString()
+
+        // TODO - we may need this later
+    }
+}
---
+++ app/src/main/java/com/siddhantkushwaha/carolyn/service/SendMessageService.kt
@@ -0,0 +1,13 @@
+package com.siddhantkushwaha.carolyn.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class SendMessageService : Service() {
+
+    override fun onBind(intent: Intent?): IBinder? {
+
+        return null
+    }
+}
