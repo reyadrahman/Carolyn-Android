49a55ed46a53e87327bb36a7a27e12553b59d21f
siddhantkushwaha <k16.siddhant@gmail.com>
2020-12-07 20:17:49 +0530

    This should fix the bug :/

--- app/src/main/java/com/siddhantkushwaha/carolyn/service/SMSReceiver.kt
+++ app/src/main/java/com/siddhantkushwaha/carolyn/service/SMSReceiver.kt
@@ -18,6 +18,8 @@ import java.time.Instant
 
 class SMSReceiver : BroadcastReceiver() {
 
+    private val tag = "SMSReceiver"
+
     override fun onReceive(context: Context, intent: Intent) {
 
         val tag = this::class.java.toString()
@@ -25,88 +27,111 @@ class SMSReceiver : BroadcastReceiver() {
         if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION == intent.action) {
 
             // save new messages in local database
-
             IndexTask(context, true).start()
 
+            var user2 = ""
+            var messageBody = ""
+            var timestampMillis = -1L
             for (smsMessage in Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
-                val user2 = smsMessage.originatingAddress?.replace("-", "") ?: continue
+                if (timestampMillis == -1L || timestampMillis != smsMessage.timestampMillis) {
+                    if (timestampMillis > -1L)
+                        processMessage(context, user2, messageBody, timestampMillis)
 
-                // thread to classify and send notification
-                val thread = Thread {
+                    // resetting all the values
+                    user2 = smsMessage.originatingAddress?.replace("-", "") ?: continue
+                    messageBody = smsMessage.messageBody
+                    timestampMillis = smsMessage.timestampMillis
+                } else {
+                    // SmsMessage is f***ing buggy, this is a workaround
+                    messageBody += smsMessage.messageBody
+                }
+            }
 
-                    val realm = RealmUtil.getCustomRealmInstance(context)
+            if (timestampMillis > -1L)
+                processMessage(context, user2, messageBody, timestampMillis)
+        }
+    }
 
-                    val contact =
-                        realm.where(Contact::class.java).equalTo("number", user2).findFirst()
+    private fun processMessage(
+        context: Context,
+        user2: String,
+        messageBody: String,
+        timestampMillis: Long
+    ) {
+        // thread to classify and send notification
+        val thread = Thread {
 
-                    val messageClass =
+            val realm = RealmUtil.getCustomRealmInstance(context)
 
-                        // If message is in contacts, always treat all messages as personal
-                        if (contact != null) {
-                            null
-                        }
+            val contact =
+                realm.where(Contact::class.java).equalTo("number", user2).findFirst()
 
-                        // If number has 10 digits, we have decided to mark the message as personal
-                        else if (user2.length == 13) {
-                            null
-                        }
+            val messageClass =
 
-                        // If prediction needs to be applied
-                        else {
-
-                            // If language is not english, mark it spam
-                            if (LanguageId.getLanguage(smsMessage.messageBody) != LanguageType.en) {
-                                MessageType.spam
-                            }
-
-                            // Use model
-                            else {
-                                MessageClassifier.doClassification(
-                                    context,
-                                    smsMessage.messageBody,
-                                    skipIfNotDownloaded = true
-                                )
-                            }
-                        }
-
-                    // Details required for sending notification
-                    val photoUri = contact?.photoUri
-                    val user2DisplayName = contact?.name ?: user2
-                    val trimmedMessage =
-                        if (smsMessage.messageBody.length > 300)
-                            "${smsMessage.messageBody.substring(0, 300)}..."
-                        else
-                            smsMessage.messageBody
-
-                    // This is to capture a mysterious bug, some notifications have trimmed text
-                    val notificationLog = LogMessage()
-                    notificationLog.log =
-                        "originalMessage=${smsMessage.messageBody};trimmedMessage=$trimmedMessage"
-                    notificationLog.timestamp = Instant.now().toEpochMilli()
-                    realm.executeTransaction {
-                        it.insertOrUpdate(notificationLog)
-                    }
-
-                    realm.close()
-
-                    Log.d(tag, "${smsMessage.messageBody} - $messageClass")
-
-                    // This workaround should do for now
-                    val notificationId = smsMessage.timestampMillis.toInt()
-
-                    val notificationSender = NotificationSender(context)
-                    notificationSender.sendNotification(
-                        notificationId,
-                        user2,
-                        user2DisplayName,
-                        trimmedMessage,
-                        photoUri,
-                        messageClass
-                    )
+                // If message is in contacts, always treat all messages as personal
+                if (contact != null) {
+                    null
                 }
 
-                thread.start()
+                // If number has 10 digits, we have decided to mark the message as personal
+                else if (user2.length == 13) {
+                    null
+                }
+
+                // If prediction needs to be applied
+                else {
+
+                    // If language is not english, mark it spam
+                    if (LanguageId.getLanguage(messageBody) != LanguageType.en) {
+                        MessageType.spam
+                    }
+
+                    // Use model
+                    else {
+                        MessageClassifier.doClassification(
+                            context,
+                            messageBody,
+                            skipIfNotDownloaded = true
+                        )
+                    }
+                }
+
+            // Details required for sending notification
+            val photoUri = contact?.photoUri
+            val user2DisplayName = contact?.name ?: user2
+            val trimmedMessage =
+                if (messageBody.length > 300)
+                    "${messageBody.substring(0, 300)}..."
+                else
+                    messageBody
+
+            // This is to capture a mysterious bug, some notifications have trimmed text
+            val notificationLog = LogMessage()
+            notificationLog.log =
+                "originalMessage=${messageBody};trimmedMessage=$trimmedMessage"
+            notificationLog.timestamp = Instant.now().toEpochMilli()
+            realm.executeTransaction {
+                it.insertOrUpdate(notificationLog)
             }
+
+            realm.close()
+
+            Log.d(tag, "$messageBody - $messageClass")
+
+            // This workaround should do for now
+            val notificationId = timestampMillis.toInt()
+
+            val notificationSender = NotificationSender(context)
+            notificationSender.sendNotification(
+                notificationId,
+                user2,
+                user2DisplayName,
+                trimmedMessage,
+                photoUri,
+                messageClass
+            )
         }
+
+        thread.start()
     }
 }
