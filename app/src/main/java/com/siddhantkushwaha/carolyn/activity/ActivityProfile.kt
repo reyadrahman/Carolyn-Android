21b013f3b8994c710a303599d42315606b930b63
siddhantkushwaha <k16.siddhant@gmail.com>
2020-12-09 10:04:24 +0530

    adding profile activity for rules

---
+++ app/src/main/java/com/siddhantkushwaha/carolyn/activity/ActivityProfile.kt
@@ -0,0 +1,12 @@
+package com.siddhantkushwaha.carolyn.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.siddhantkushwaha.carolyn.R
+
+class ActivityProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
