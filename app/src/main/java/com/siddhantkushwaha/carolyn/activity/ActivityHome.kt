b0362887c482a49e2f1588106ffada76e9eb471f
siddhantkushwaha <k16.siddhant@gmail.com>
2021-02-07 13:09:36 +0530

    Layout error fixes

--- app/src/main/java/com/siddhantkushwaha/carolyn/activity/ActivityHome.kt
+++ app/src/main/java/com/siddhantkushwaha/carolyn/activity/ActivityHome.kt
@@ -5,7 +5,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.PowerManager
 import android.provider.Settings
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
